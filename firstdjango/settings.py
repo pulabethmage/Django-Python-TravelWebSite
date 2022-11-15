6b2163a2cf4502504ff889c203d942ee26149d90
Pulasthi Bethmage <pulasthibethmage@gmail.com>
2022-11-15 17:08:48 +0530

    Database connection done, Registration done,Login done, Logout done

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travel.apps.TravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'firstdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travelapp',
+        'USER' : 'postgres',
+        'PASSWORD' : '123',
+        'HOST' : 'localhost'
     }
 }
 
@@ -121,6 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
